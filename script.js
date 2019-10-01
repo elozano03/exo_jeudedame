96446d65d5c0c8c504c0f7aa81d6b5a0d188711f
Lozano Elian <Elian.lozano@outlook.fr>
2019-10-01 12:26:01 +0200

    OUI

--- script.js
+++ script.js
@@ -3,22 +3,30 @@ var container = document.createElement("table");
 container.setAttribute("id" , "plate");
 document.body.appendChild(container)
 
+
+
 for (let i = 0; i < 10; i++) {
-    var horizontal = document.createElement("tr")
-    container.appendChild(horizontal)
+    var tr = document.createElement("tr")
+    container.appendChild(tr)
     for (let j = 0; j < 10; j++) {
-        var vertical = document.createElement("td")
-        horizontal.appendChild(vertical)
+        var td = document.createElement("td")
+        tr.appendChild(td)
         if (i % 2 == j % 2) {
-            vertical.style.backgroundColor = ("white")
+            td.style.backgroundColor = ("white")
         }
         else {
-            horizontal.style.backgroundColor = ("black")
+            if (i < 4) {  
+            var redpion = document.createElement("div")
+            redpion.setAttribute("id", "red")
+            td.appendChild(redpion)
+            } if (i > 5){
+                var whitepion = document.createElement("div")
+                whitepion.setAttribute("id", "white")
+                td.appendChild(whitepion)
+            }
+
+            tr.style.backgroundColor = ("black")
         }
  
-        
-    }
-    
+    } 
 }
-    
-
